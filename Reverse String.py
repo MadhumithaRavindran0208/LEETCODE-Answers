74451e75bd172299bd44e7f44d12a67c5dd3d16b
Madhumitha Ravindran <ammumadhu0208@gmail.com>
2026-02-21 23:49:24 +0530

    Add files via upload
    Two approaches to reverse a list in-place: the first uses Python's built-in .reverse() method (though it returns None), while the second correctly uses slice assignment s[:] = s[::-1] to reverse the list in-place and returns the modified list.

---
+++ Reverse String.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def reverseString(self, s):
+        return(s.reverse())
+class Solution(object):
+    def reverseString(self, s):
+        s[:]=s[::-1]
+        return(s)
+              
