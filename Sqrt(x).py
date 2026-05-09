410f7584ff41b6922cfc9028515db00aa2aca8e4
Madhumitha Ravindran <ammumadhu0208@gmail.com>
2026-05-09 22:41:19 +0530

    Refactor mySqrt to use binary search method

--- Sqrt(x).py
+++ Sqrt(x).py
@@ -1,3 +1,16 @@
 class Solution(object):
     def mySqrt(self, x):
         return(int(x**0.5))
+class Solution(object):
+    def mySqrt(self, x):
+        low = 0
+        high = x
+        while high >= low:
+            mid = (high + low) / 2
+            if mid * mid < x:
+                low = mid + 1
+            elif mid * mid > x:
+                high = mid - 1
+            else:
+                return mid
+        return mid if mid * mid < x else mid - 1
