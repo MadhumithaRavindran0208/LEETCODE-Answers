53c365c2d2d06b1ade601690f77fc0902b0b57dc
Madhumitha Ravindran <ammumadhu0208@gmail.com>
2026-05-09 22:31:12 +0530

    Refactor searchRange to use binary search for efficiency

--- Find First and Last Position of Element in Sorted Array.py
+++ Find First and Last Position of Element in Sorted Array.py
@@ -14,6 +14,38 @@ class Solution(object):
         l=[-1,-1]
         if target in nums:
             l[0]=nums.index(target)
-            for i in range (len(nums)):
-                if nums[i]==target:l[1]=i
+            for i in range (len(nums)-1,-1,-1):
+                if nums[i]==target:
+                    l[1]=i
+                    break
         return l
+class Solution(object):
+    def searchRange(self, nums, target):
+        def findLeft():
+            left, right = 0, len(nums) - 1
+            res = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    res = mid
+                    right = mid - 1  # keep searching left
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return res
+        def findRight():
+            left, right = 0, len(nums) - 1
+            res = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    res = mid
+                    left = mid + 1  # keep searching right
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return res
+        return [findLeft(), findRight()]
+        
