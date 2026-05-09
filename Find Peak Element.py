43ee1b48877b96fd1df378b7a8e044c74a5032bd
Madhumitha Ravindran <ammumadhu0208@gmail.com>
2026-05-10 00:52:36 +0530

    Update Find Peak Element.py

--- Find Peak Element.py
+++ Find Peak Element.py
@@ -1,4 +1,13 @@
 class Solution(object):
     def findPeakElement(self, nums):
         return (nums.index(max(nums)))
-        
+class Solution(object):
+    def findPeakElement(self, nums):   
+        begin = 0
+        end = len(nums)-1
+        while begin < end:
+            if nums[begin] < nums[end]:
+                begin += 1
+            else:
+                end -= 1
+        return begin
