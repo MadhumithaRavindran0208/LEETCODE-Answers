22f53dfd655cb6653045851cc1d6b210b88c306f
Madhumitha Ravindran <ammumadhu0208@gmail.com>
2026-06-12 00:51:52 +0530

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -6,3 +6,8 @@ class Solution(object):
                     return(i,j)
                 else:
                     continue
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            c=target-nums[i]
+            if c in nums[i+1:]:return [i,nums.index(c,i+1)]
