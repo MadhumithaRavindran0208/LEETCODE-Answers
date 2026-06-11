25be0d7b3b2515c610bf9409c37340e623df978b
Madhumitha Ravindran <ammumadhu0208@gmail.com>
2026-06-11 22:31:32 +0530

    Update Rotate Image.py

--- Rotate Image.py
+++ Rotate Image.py
@@ -6,4 +6,16 @@ class Solution(object):
             mat1.append(list(i[::-1]))
         matrix[:]=mat1[:]
         return (matrix)
+class Solution(object):
+    def rotate(self, matrix):
+        n=len(matrix)
+        mat=[]
+        for i in range(n):
+            a=[]
+            for j in range(n-1,-1,-1):
+                a.append(matrix[j][i])
+            mat.append(a)
+        for i in range(n):
+            matrix[i][:]=mat[i]
+        return matrix
 
