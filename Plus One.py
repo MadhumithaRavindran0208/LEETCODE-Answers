d6782bd3cf3e965823ee349addd8de496818a18e
Madhumitha Ravindran <ammumadhu0208@gmail.com>
2026-06-18 01:27:26 +0530

    Update Plus One.py

--- Plus One.py
+++ Plus One.py
@@ -9,4 +9,14 @@ class Solution(object):
         for i in (str(a)):
             list_a.append(int(i))
         return(list_a)
+class Solution(object):
+    def plusOne(self, digits):
+        digit=""
+        for i in digits:
+            digit=digit+str(i)
+        digit=int(digit)+1
+        final=[]
+        for i in str(digit):
+            final.append(int(i))
+        return final
         
