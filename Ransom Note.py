b309204883d06894c9c4e2e9554406a5a571b575
Madhumitha Ravindran <ammumadhu0208@gmail.com>
2026-06-20 01:06:05 +0530

    Update Ransom Note.py

--- Ransom Note.py
+++ Ransom Note.py
@@ -4,4 +4,16 @@ class Solution(object):
         for i in c:
             if ransomNote.count(i)>magazine.count(i):return False
         else:return True
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        c=set(ransomNote)
+        m={}
+        for i in magazine:
+            m[i]=m.get(i,0)+1
+        r={}
+        for i in ransomNote:
+            r[i]=r.get(i,0)+1
+        for i in c:
+            if m.get(i,0)<r[i]:return False
+        else:return True
         
