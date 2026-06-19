989bd9c019b89071de4c1ea28c0e4b0343e325ee
Madhumitha Ravindran <ammumadhu0208@gmail.com>
2026-06-20 00:48:37 +0530

    Refactor runningSum method for efficiency

--- Running Sum of 1d Array.py
+++ Running Sum of 1d Array.py
@@ -4,4 +4,10 @@ class Solution(object):
         for i in range (1,len(nums)+1):
             l.append(sum(nums[:i]))
         return (l)
-        
+class Solution(object):
+    def runningSum(self, nums):
+        l=[nums[0]]
+        for i in range (1,len(nums)):
+            l.append(l[i-1]+nums[i])
+        return (l)
+                
