d976c7cb73f526c676a25171ff892dd7536c7f67
Madhumitha Ravindran <ammumadhu0208@gmail.com>
2026-06-19 23:56:31 +0530

    Add files via upload
    Maximum Number of Balloons – finds how many times the word "balloon" can be formed from the letters in a given string. Uses Counter to tally character frequencies, then determines the limiting factor by dividing the counts of doubled letters ('l', 'o') by 2 and taking the minimum across all required letters. | Python | LeetCode 1189

---
+++ Maximum Number of Balloons.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def maxNumberOfBalloons(self, text):
+        from collections import Counter
+        count=Counter(text)
+        return min(count["b"],count["a"],count["l"]/2,count["o"]/2,count["n"])
+        
