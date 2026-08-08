344bd09409b4b47fa1896a575ab5d2701e75d37b
Madhumitha Ravindran <ammumadhu0208@gmail.com>
2026-08-08 20:46:30 +0530

    Add files via upload
    Greatest Common Divisor of Strings – finds the largest string that can divide both input strings evenly through repetition. Checks whether the two strings are "rotationally compatible" (str1+str2 equals str2+str1), and if so, returns the prefix of length equal to the GCD of their lengths. | Python | LeetCode 1071

---
+++ Greatest Common Divisor of Strings.py
@@ -0,0 +1,7 @@
+from fractions import gcd
+
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        if str1+str2!=str2+str1:
+            return ""
+        return str1[:gcd(len(str1), len(str2))]
